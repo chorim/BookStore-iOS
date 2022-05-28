cdcb6fefbfe02c5f5849a731459179c364f65fa3
Insu Byeon <yuki@yuki.kr>
2022-05-28 23:44:29 +0900

    Upload project

---
+++ Targets/BookStoreKit/Sources/BookStoreKit.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+public final class BookStoreKit {
+    public static func hello() {
+        print("Hello, from your Kit framework")
+    }
+}
---
+++ Targets/BookStoreUI/Tests/BookStoreUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class BookStoreUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("BookStoreUI", "BookStoreUI")
+    }
+}
