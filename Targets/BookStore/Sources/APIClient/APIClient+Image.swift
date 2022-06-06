5273bdf6e7b20fa13c382b5f28fb8c962f14ba5b
Insu Byeon <yuki@yuki.kr>
2022-06-07 00:50:25 +0900

    add Image Cache from memory

--- Targets/BookStore/Sources/APIClient/APIClient+Image.swift
+++ Targets/BookStore/Sources/APIClient/APIClient+Image.swift
@@ -9,33 +9,74 @@
 import Foundation
 import UIKit
 
+extension UIImage {
+  enum Scale: CGFloat {
+    case low = 0.25
+    case medium = 0.50
+    case high = 0.75
+    case original = 1.0
+  }
+  
+  convenience init?(data: Data, scale: Scale = .high) {
+    self.init(data: data, scale: scale.rawValue)
+  }
+}
+
 extension APIClient {
-  func requestImage(_ imageUrl: String) async throws -> Data? {
+  func requestImage(_ imageUrl: String) async throws -> UIImage? {
     guard let imageURL = URL(string: imageUrl) else {
       throw URLError(.badURL)
     }
     
     let (data, _) = try await session.data(from: imageURL)
-    return data
+    return UIImage(data: data)
   }
 }
 
 extension UIImageView {
-  func setImage(_ remoteUrl: String) {
+  func setImage(_ imageUrl: String) {
     Task.detached(priority: .utility) {
       try Task.checkCancellation()
-      let data = try await APIClient.shared.requestImage(remoteUrl)
-      try Task.checkCancellation()
-      await self.setImage(data)
+      
+      guard let imageURL = URL(string: imageUrl) else {
+        throw URLError(.badURL)
+      }
+      
+      let cacheKey = imageURL.lastPathComponent
+      
+      if let image = ImageCacheProvider.shared.get(key: cacheKey) {
+        await self.setImage(image)
+      } else {
+        let image = try await APIClient.shared.requestImage(imageUrl)
+        try Task.checkCancellation()
+        await self.setImage(image)
+        if let image = image {
+          ImageCacheProvider.shared.put(cacheKey, image: image)
+        }
+      }
     }
   }
   
   @MainActor
-  func setImage(_ data: Data?) {
-    if let data = data {
-      self.image = UIImage(data: data, scale: 0.75)
-    }
+  func setImage(_ image: UIImage?) {
+    self.image = image
   }
   
   func cancel() {}
+}
+
+// MARK: ImageCacheProvider
+final class ImageCacheProvider {
+  private let cache = NSCache<NSString, UIImage>()
+  private init() {}
+  
+  static let shared = ImageCacheProvider()
+  
+  func put(_ key: String, image: UIImage) {
+    cache.setObject(image, forKey: key as NSString)
+  }
+  
+  func get(key: String) -> UIImage? {
+    return cache.object(forKey: key as NSString)
+  }
 }
